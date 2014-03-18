aeae9c6776b5e49dae7c602283738d5f9c165b34
Ian Temple <ianrtemple@googlemail.com>
2014-03-18 10:29:28 +0000

    Stuff

--- Exercise5/src/model/PerfectAction.java
+++ Exercise5/src/model/PerfectAction.java
@@ -1,5 +1,80 @@
 package model;
 
-public class PerfectAction {
+import rp.robotics.localisation.ActionModel;
+import rp.robotics.localisation.GridPositionDistribution;
+import rp.robotics.mapping.Heading;
 
+public class PerfectAction implements ActionModel {
+
+	@Override
+	public GridPositionDistribution updateAfterMove(
+			GridPositionDistribution _from, Heading _heading) {
+
+		// Create the new distribution that will result from applying the action
+		// model
+		GridPositionDistribution to = new GridPositionDistribution(_from);
+
+		// Move the probability in the correct direction for the action
+		if (_heading == Heading.PLUS_X) {
+			movePlusX(_from, to);
+		} else if (_heading == Heading.PLUS_Y) {
+			// you could implement a movePlusY etc. or you could find a way do
+			// do all moves in a single method. Hint: all changes are just + or
+			// - 1 to an x or y value.
+		} else if (_heading == Heading.MINUS_X) {
+
+		} else if (_heading == Heading.MINUS_Y) {
+
+		}
+
+		return to;
+	}
+
+	/**
+	 * Move probabilities from _from one cell in the plus x direction into _to
+	 * 
+	 * @param _from
+	 * @param _to
+	 */
+	private void movePlusX(GridPositionDistribution _from,
+			GridPositionDistribution _to) {
+
+		// iterate through points updating as appropriate
+		for (int y = 0; y < _to.getGridHeight(); y++) {
+
+			for (int x = 0; x < _to.getGridWidth(); x++) {
+				float oldP = _to.getProbability(x, y);
+				_to.setProbability(x, y, );
+				
+				// make sure to respect obstructed grid points
+				if (!_to.isObstructed(x, y)) {
+
+					// the action model should work out all of the different
+					// ways (x,y) in the _to grid could've been reached based on
+					// the _from grid and the move taken (in this case
+					// HEADING.PLUS_X)
+
+					// for example if the only way to have got to _to (x,y) was
+					// from _from (x-1, y) (i.e. there was a PLUS_X move from
+					// (x-1, y) then you write that to the (x, y) value
+
+					// The below code does not move the value, just copies
+					// it to the same position
+
+					// position before move
+					int fromX = x;
+					int fromY = y;
+					float fromProb = _from.getProbability(fromX, fromY);
+
+					// position after move
+					int toX = x;
+					int toY = y;
+
+					// set probability for position after move
+					_to.setProbability(toX, toY, fromProb);
+
+				}
+			}
+		}
+	}
 }
