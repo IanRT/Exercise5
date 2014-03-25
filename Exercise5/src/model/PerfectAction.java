209c4e564798b57e125d0a57b414359de30686dc
Ian Temple <ianrtemple@googlemail.com>
2014-03-25 15:50:06 +0000

    Localiser class created, runs with moving probabilities, currently bug fixing

--- Exercise5/src/model/PerfectAction.java
+++ Exercise5/src/model/PerfectAction.java
@@ -18,13 +18,11 @@ public class PerfectAction implements ActionModel {
 		if (_heading == Heading.PLUS_X) {
 			movePlusX(_from, to);
 		} else if (_heading == Heading.PLUS_Y) {
-			// you could implement a movePlusY etc. or you could find a way do
-			// do all moves in a single method. Hint: all changes are just + or
-			// - 1 to an x or y value.
+			movePlusY(_from, to);
 		} else if (_heading == Heading.MINUS_X) {
-
+			moveMinusX(_from, to);
 		} else if (_heading == Heading.MINUS_Y) {
-
+			moveMinusY(_from, to);
 		}
 
 		return to;
@@ -43,8 +41,57 @@ public class PerfectAction implements ActionModel {
 		for (int y = 0; y < _to.getGridHeight(); y++) {
 
 			for (int x = 0; x < _to.getGridWidth(); x++) {
-				float oldP = _to.getProbability(x, y);
-				_to.setProbability(x, y, );
+				
+				// make sure to respect obstructed grid points
+				if (!_to.isObstructed(x, y)) {
+
+					// the action model should work out all of the different
+					// ways (x,y) in the _to grid could've been reached based on
+					// the _from grid and the move taken (in this case
+					// HEADING.PLUS_X)
+
+					// for example if the only way to have got to _to (x,y) was
+					// from _from (x-1, y) (i.e. there was a PLUS_X move from
+					// (x-1, y) then you write that to the (x, y) value
+
+					// The below code does not move the value, just copies
+					// it to the same position
+
+					// position before move
+					float fromProb =0;
+					int fromX = x;
+					int fromY = y;
+					
+					if(_from.isValidGridPoint(fromX, fromY)){
+						fromProb = _from.getProbability(fromX, fromY);
+					}
+					
+					// position after move
+					int toX = x+1;
+					int toY = y;
+					
+					
+					if(!_to.isValidTransition(fromX, fromY, toX, toY))
+					{
+						_to.setProbability(fromX, fromY, fromProb+_from.getProbability(fromX, fromY));
+					}
+					else
+					{
+						_to.setProbability(toX, toY, fromProb);
+						if(!_to.isValidTransition(fromX-1, fromY, fromX, fromY))
+							_to.setProbability(fromX, fromY, 0);
+					}
+				}
+			}
+		}
+	}
+	private void movePlusY(GridPositionDistribution _from,
+			GridPositionDistribution _to) {
+
+		// iterate through points updating as appropriate
+		for (int y = 0; y < _to.getGridHeight(); y++) {
+
+			for (int x = 0; x < _to.getGridWidth(); x++) {
 				
 				// make sure to respect obstructed grid points
 				if (!_to.isObstructed(x, y)) {
@@ -63,6 +110,47 @@ public class PerfectAction implements ActionModel {
 
 					// position before move
 					int fromX = x;
+					int fromY = y-1;
+					float fromProb = _from.getProbability(fromX, fromY);
+
+					// position after move
+					int toX = x;
+					int toY = y;
+
+					if(_to.isObstructed(toX, toY+1))
+						_to.setProbability(toX, toY, fromProb+_from.getProbability(toX, toY));
+					else
+						_to.setProbability(toX, toY, fromProb);
+
+				}
+			}
+		}
+	}
+	private void moveMinusX(GridPositionDistribution _from,
+			GridPositionDistribution _to) {
+
+		// iterate through points updating as appropriate
+		for (int y = 0; y < _to.getGridHeight(); y++) {
+
+			for (int x = 0; x < _to.getGridWidth(); x++) {
+				
+				// make sure to respect obstructed grid points
+				if (!_to.isObstructed(x, y)) {
+
+					// the action model should work out all of the different
+					// ways (x,y) in the _to grid could've been reached based on
+					// the _from grid and the move taken (in this case
+					// HEADING.PLUS_X)
+
+					// for example if the only way to have got to _to (x,y) was
+					// from _from (x-1, y) (i.e. there was a PLUS_X move from
+					// (x-1, y) then you write that to the (x, y) value
+
+					// The below code does not move the value, just copies
+					// it to the same position
+
+					// position before move
+					int fromX = x+1;
 					int fromY = y;
 					float fromProb = _from.getProbability(fromX, fromY);
 
@@ -70,8 +158,51 @@ public class PerfectAction implements ActionModel {
 					int toX = x;
 					int toY = y;
 
-					// set probability for position after move
-					_to.setProbability(toX, toY, fromProb);
+					if(_to.isObstructed(toX-1, toY))
+						_to.setProbability(toX, toY, fromProb+_from.getProbability(toX, toY));
+					else
+						_to.setProbability(toX, toY, fromProb);
+
+				}
+			}
+		}
+	}
+	private void moveMinusY(GridPositionDistribution _from,
+			GridPositionDistribution _to) {
+
+		// iterate through points updating as appropriate
+		for (int y = 0; y < _to.getGridHeight(); y++) {
+
+			for (int x = 0; x < _to.getGridWidth(); x++) {
+				
+				// make sure to respect obstructed grid points
+				if (!_to.isObstructed(x, y)) {
+
+					// the action model should work out all of the different
+					// ways (x,y) in the _to grid could've been reached based on
+					// the _from grid and the move taken (in this case
+					// HEADING.PLUS_X)
+
+					// for example if the only way to have got to _to (x,y) was
+					// from _from (x-1, y) (i.e. there was a PLUS_X move from
+					// (x-1, y) then you write that to the (x, y) value
+
+					// The below code does not move the value, just copies
+					// it to the same position
+
+					// position before move
+					int fromX = x;
+					int fromY = y+1;
+					float fromProb = _from.getProbability(fromX, fromY);
+
+					// position after move
+					int toX = x;
+					int toY = y;
+
+					if(_to.isObstructed(toX, toY-1))
+						_to.setProbability(toX, toY, fromProb+_from.getProbability(toX, toY));
+					else
+						_to.setProbability(toX, toY, fromProb);
 
 				}
 			}
